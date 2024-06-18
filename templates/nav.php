0e200ad9638687aeab35c24b8009dfe9c732ddba
ShirleenMuthoni <shirleenwanyingi@gmail.com>
2024-06-18 06:35:12 +0300

    created a file to contain the navigations

---
+++ templates/nav.php
@@ -0,0 +1,12 @@
+<div class="topnav">
+     <a href="./">Home</a> 
+     <a href="highlight.php">Highlights</a> 
+     <a href="models.php">Models</a>
+     <a href="technology.php" >Technology</a>
+     <a href="gallery.php" >Gallery</a>
+     <a href="events.php" >Events</a>
+
+    <div class="topnav-right">
+    <a href="signup.php" >Sign Up</a>
+    </div>
+    </div>
