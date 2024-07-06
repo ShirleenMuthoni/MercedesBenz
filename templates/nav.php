23213963e66277320fed8cbf6031c9e3003742ac
ShirleenMuthoni <shirleenwanyingi@gmail.com>
2024-07-06 21:35:43 +0300

    renamed all my pages

--- templates/nav.php
+++ templates/nav.php
@@ -1,10 +1,10 @@
 <div class="topnav">
      <a href="./">Home</a> 
-     <a href="highlight.php">Highlights</a> 
-     <a href="models.php">Models</a>
-     <a href="technology.php" >Technology</a>
-     <a href="gallery.php" >Gallery</a>
-     <a href="events.php" >Events</a>
+     <a href="C-Class.php">Highlights</a> 
+     <a href="G-Class.php">Models</a>
+     <a href="S-Class.php" >Technology</a>
+     <a href="AMG-GLE.php" >Gallery</a>
+     <a href="AMG SL-Class.php" >Events</a>
 
     <div class="topnav-right">
     <a href="signup.php" >Sign Up</a>
