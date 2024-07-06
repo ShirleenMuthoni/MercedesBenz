1c8841eb0f75e5338316ed1648a0ad1c0a11a8b3
ShirleenMuthoni <shirleenwanyingi@gmail.com>
2024-07-06 21:24:55 +0300

    created a G-Class page

---
+++ G-Class.php
@@ -0,0 +1,10 @@
+<?php include_once ("templates/heading.php");?>
+<?php include_once ("templates/nav.php");?>
+<body>
+  
+
+   
+    <?php include_once("templates/footer.php");?>
+
+</body>
+</html>
